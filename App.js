e4e3582947d19760079a517ab04903c630e53a86
CleigloDev <carlolune@gmail.com>
2020-02-25 22:13:53 +0100

    add custom icon back

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, StatusBar, Text} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from './config.json';
+const Icon = createIconSetFromFontello(fontelloConfig);
 
 const App = () => {
+  
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Text style={{fontFamily: "OpenSans-Bold"}}>Ciao</Text>
-        <Icon name="rocket" size={80} color="#bf1313" />
+        <Icon name="back" size={20} color="#bf1313" />
       </SafeAreaView>
     </>
   );
